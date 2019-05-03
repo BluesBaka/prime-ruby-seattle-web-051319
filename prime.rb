f3300297eb5bdaa5135f2be83d03f462e72eea5e
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 03:01:26 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 def prime?(i)
-    if i < 1 false
-    
-    (2..i-1).each do |number| return false if i % number === 0
+
+    (2..i-1).each do |number| return false if i % number === 0 || i< 1
     end
     true
 end
