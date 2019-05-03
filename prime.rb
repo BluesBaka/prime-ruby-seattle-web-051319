d1a8dcd873c9c5e92f7681fe65dfde9fe607bad3
Bryna Dunnells <badunnells@gmail.com>
2019-05-03 03:19:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 def prime?(i)
-    if i <= 1 return false
-    else
+    if i <= 1 
+      return false
+    end
 
     (2..i-1).each do |number| return false if i % number === 0
     end
-  end
+  
     true
 end
 # Add  code here!
